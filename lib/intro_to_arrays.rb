f0f7d6f44264616562fd22ecccb312a1e7f4cef4
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-21 22:43:57 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,3 +11,22 @@ def first_element(name)
   return name[0]
 end  
 
+def third_element(name)
+  return name[2]
+end  
+
+def last_element(name)
+  return name[-1]
+end  
+
+def first_element_with_array_methods(name)
+  return name.first  
+end
+
+def last_element_with_array_methods(name)
+  return name.last 
+end
+
+def length_of_array(name)
+  return name.length 
+end  
